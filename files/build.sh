4b5b6970e4ee5f8470c08e2ceb4bf4d796f92f9f
Trevor K Smith <trevorksmith@protonmail.com>
2023-11-06 10:04:16 +0000

    Update 2 files
    - /files/build.sh
    - /playbooks/firedancer.yml

--- files/build.sh
+++ files/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e  # Exit on error
 
+#Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+
 # Set environment variables
 export FD_AUTO_INSTALL_PACKAGES=1
 
